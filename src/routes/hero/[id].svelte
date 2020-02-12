6d8411062cd477c1aae0bce959011c8a99831e95
Eugene Mirotin <emirotin@gmail.com>
2020-02-12 14:06:19 +0200

    init

---
+++ src/routes/hero/[id].svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  export function preload({ params }) {
+    return { id: params.id };
+  }
+</script>
+
+<script>
+  export let id;
+</script>
+
+<h1>Hero {id}</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Marvel Heroes</title>
+</svelte:head>
+
+<main>
+  SEARCH
+  <a href="/hero/1234">Hero 1234</a>
+</main>
